bd46a02f4b0ca36321c651d32a9dcd8545ee89fc
Alfred Tuinman <alfred@ourmaninindia.com>
2021-06-16 12:07:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 🚀 Travel Information Management
+
+This project has been created using **webpack-cli**. You can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application.
+
+For the development version run 
+```
+npm run build:dev
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebPackPlugin from "html-webpack-plugin"
+
+export default {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.join('/data/webapps/backoffice/', 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  target: 'web',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        // Loads the javacript into html template provided.
+        // Entry point is set below in HtmlWebPackPlugin in Plugins 
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            //options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+       test: /\.(png|svg|jpg|gif)$/,
+       use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/html/index.html",
+      filename: "./index.html",
+      excludeChunks: [ 'server' ]
+    })
+  ]
+}
